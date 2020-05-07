029a5dc65b463f2ac5f22e8adb87bf92e301f806
Siper <siper13@gmail.com>
2020-05-07 19:49:14 +0300

    IOException instead Exception

--- library/rtsp/src/main/java/com/google/android/exoplayer2/source/rtsp/core/ClientException.java
+++ library/rtsp/src/main/java/com/google/android/exoplayer2/source/rtsp/core/ClientException.java
@@ -1,28 +1,30 @@
 package com.google.android.exoplayer2.source.rtsp.core;
 
-public class ClientException extends Exception {
+import java.io.IOException;
+
+public class ClientException extends IOException {
 
   private ClientException(String message) {
     super(message);
   }
 
-  public static Exception timeoutException() {
+  public static IOException timeoutException() {
     return new ClientException("Client timeout exception");
   }
 
-  public static Exception ioException() {
+  public static IOException ioException() {
     return new ClientException("IO exception");
   }
 
-  public static Exception malformedResponseException() {
+  public static IOException malformedResponseException() {
     return new ClientException("Malformed response exception");
   }
 
-  public static Exception unknownException() {
+  public static IOException unknownException() {
     return new ClientException("Unknown client exception");
   }
 
-  public static Exception noResponseException() {
+  public static IOException noResponseException() {
     return new ClientException("No response exception");
   }
 }
